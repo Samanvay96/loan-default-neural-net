1ff1ddf5510ea23b068adf0df18d904c3449b2e3
Tom McKeesick <tmck01@gmail.com>
2018-10-30 18:30:36 +1100

    Improve variable names and add docstrings
    * I'm super bad at stats, so most of the docstrings will be wrong
    * Use more generic variable names where applicable

--- bin/neural.py
+++ bin/neural.py
@@ -5,44 +5,47 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report,confusion_matrix
 
 def read_dataset(fpath, y_feature):
-    wine = pd.read_csv(fpath).fillna(0)
-    wine = convert_categoricals(wine)
+    'Reads a CSV file into a pandas dataframe'
+    dataset = pd.read_csv(fpath).fillna(0)
+    dataset = convert_categoricals(dataset)
 
     # Create X and Y datasets
-    X = wine.drop(y_feature,axis=1)
-    y = wine[y_feature]
+    X = dataset.drop(y_feature,axis=1)
+    y = dataset[y_feature]
     return X, y
 
-# Split dataset into test and train
 def split_dataset(X, y):
+    'Split the dataset into test and train'
     X_train, X_test, y_train, y_test = train_test_split(X, y)
     return X_train, X_test, y_train, y_test
 
 def scale_features(X_train, X_test):
+    'Do stuff'
     # Scale Features
     scaler = StandardScaler()
-
     # Configure
     scaler.fit(X_train)
     StandardScaler(copy=True, with_mean=True, with_std=True)
 
     return scaler.transform(X_train), scaler.transform(X_test)
 
-# Initialise model
 def run_model(X_train, y_train):
-    mlp = MLPClassifier(hidden_layer_sizes=(3),max_iter=100)
-    mlp.fit(X_train,y_train)
-    return mlp
+    'Runs an MLP Classifier model on the training datasets and returns a fit'
+    model = MLPClassifier(hidden_layer_sizes=(3),max_iter=100)
+    model.fit(X_train,y_train)
+    return model
 
 def convert_categoricals(df):
+    'Convert dummies?'
     return pd.get_dummies(df)
 
 def run(fpath, y_feature):
+    'Run an MLP Classifier model over the fpath file for the y_feature'
     X, y = read_dataset(fpath, y_feature)
     X_train, X_test, y_train, y_test = split_dataset(X, y)
     X_train, X_test = scale_features(X_train, X_test)
-    mlp = run_model(X_train, y_train)
-    predictions = mlp.predict(X_test)
+    model = run_model(X_train, y_train)
+    predictions = model.predict(X_test)
     print(classification_report(y_test,predictions))
 
 if __name__ == '__main__':
